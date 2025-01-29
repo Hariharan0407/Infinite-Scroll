fc25001e64e056f2c3ecf272abcbd13165c620a9
Hariharan0407 <haridmt@gmail.com>
2025-01-29 20:27:13 +0530

    added an event listener to reload the images for infinite scrolling

--- infinite_scroll.js
+++ infinite_scroll.js
@@ -6,14 +6,34 @@
 //after connect with the API and making a fetch call, the API will returna list of images data in JSON format. We have to convert the JSON data
 // into javascript object. we use .json() method of response object. Note that despite the method being named json(), the result is not JSON 
 // but is instead the result of taking JSON as input and parsing it to produce a JavaScript object.
+
+const imageContainer = document.getElementById('imagecontainer')
 async function getImages(){
     const response = await fetch ('https://api.unsplash.com/photos/?client_id=WS3HmHu2MfMspYiEeKl9NaqTR-gq3bc2IoLMwUKuf7M')
     // console.log(response);
     const imagesData = await response.json()
-    // console.log(imagesData);
+    console.log(imagesData);
     imagesData.map((image)=>{
-        console.log(image.urls.full)
+        let imageUrl = image.urls.full
+        const myImageElement = document.createElement('img')
+        // console.log(myImageElement);
+        myImageElement.setAttribute('src', imageUrl)
+        // console.log(myImageElement);
+        imageContainer.append(myImageElement)
     })
 }
 
 getImages();
+
+window.addEventListener('scroll', function(){
+    let myClientHeight = document.documentElement.clientHeight
+    //clientheight is the height of the client device window
+    let myScrollHeight = document.documentElement.scrollHeight
+    //scroll height is the total height of the scroll
+    let myScrollTop = document.documentElement.scrollTop
+    //scroll top is the distance the scroll has moved from the top
+    console.log(myClientHeight,myScrollHeight,myScrollTop)
+    if(myClientHeight+myScrollTop >= myScrollHeight){
+        getImages();
+    }
+})
